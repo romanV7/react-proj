fd539ecc0d3ac8e8b657123bbe25126fcd81f1af
Roman <mister02.rv@gmail.com>
2021-01-09 23:33:19 +0200

    rewrite fn component on class style

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,53 @@
+import * as axios from 'axios'
 import React from 'react'
 
-const Users = props => {
-    if (!props.users.length) {
-        const users = [
-            {id: 1, photoUrl: '', follwed: true, fullName: 'Bob', status: 'working...', location: {city: 'S', country: 'U'}},
-            {id: 2, photoUrl: '', follwed: true, fullName: 'David', status: 'working...', location: {city: 'K', country: 'U'}},
-        ]
-        props.setUsers(users)    
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
     }
-    return (
-        <div>
-            {
-                props.users.map(u => 
-                <div key={u.id}>
-                    <span>
-                        <div>
-                            <img src={u.photoUrl} />
-                        </div>
-                        <div>
-                            {
-                                u.followed 
-                                    ? <button onClick={() => props.follow(u.id)}>Unfollow</button> 
-                                    : <button onClick={() => props.unfollow(u.id)}>Follow</button>
-                            }
-                        </div>
-                    </span>
-                    <span>
-                       <span>
-                            <div>{u.fullName}</div>
-                           <div>{u.status}</div>
-                       </span>
-                       <span>
-                            <div>{u.location.country}</div>
-                           <div>{u.location.city}</div>
-                       </span>
-                    </span>
-                </div>
-                )
-            }
-        </div>
-    )
+
+    getUsers = () => {
+        if (!this.props.users.length) {
+            axios
+                .get('https://network.example.com/users')
+                .then(({data}) => this.props.setUsers(data.items))
+        }
+    }
+    render = () => {
+        return (
+            <div>
+                <button onClick={this.getUsers}></button>
+                {
+                    this.props.users.map(u => 
+                    <div key={u.id}>
+                        <span>
+                            <div>
+                                <img src={u.photoUrl} />
+                            </div>
+                            <div>
+                                {
+                                    u.followed 
+                                        ? <button onClick={() => this.props.follow(u.id)}>Unfollow</button> 
+                                        : <button onClick={() => this.props.unfollow(u.id)}>Follow</button>
+                                }
+                            </div>
+                        </span>
+                        <span>
+                        <span>
+                                <div>{u.fullName}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                                <div>{u.location.country}</div>
+                            <div>{u.location.city}</div>
+                        </span>
+                        </span>
+                    </div>
+                    )
+                }
+            </div>
+        )
+    }
 }
 
 export default Users
