c425fa7eaa07cf941966041c7f9bc35c6360a2f8
Roman <mister02.rv@gmail.com>
2021-01-12 00:00:49 +0200

    refactor

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,14 @@
 import * as axios from 'axios'
 
+const instance = axios.create({
+    withCredentials: true,
+    headers: {
+        'API-KEY': ''
+    }
+})
+
 export const getUsers = (pagesCount) => {
-    return axios.get(`https://network.example.com/users?page=${pagesCount}`, {
-        withCredentials: true
-    })
+    return instance.get(`https://network.example.com/users?page=${pagesCount}`)
         .then(response => response.data)
 }
 
