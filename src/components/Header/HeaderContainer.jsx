0aa06559ff6b806a10d0e091867f4813688c1551
Roman <mister02.rv@gmail.com>
2021-01-12 23:45:18 +0200

    refactor

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,22 +1,12 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import Header from './Header'
-import s from './Header.module.css'
-import * as axios from 'axios'
 import { connect } from 'react-redux'
+import {authMe} from '../../redux/auth-reducer'
 
 class HeaderContainer extends React.Component {
 
     componentDidMount() {
-        axios.get(`https://network.example.com/auth/me`, {
-            withCredentials: true
-        })
-            .then(response => {
-                if (response.data.resultCode === 0) {
-                    const {id, email, login} = response.data.data
-                    this.props.setUserData({id, email, login})
-                }
-            })
+        this.props.authMe()
     }
 
     render() {
@@ -29,4 +19,4 @@ const mapStateToProps = state => ({
     login: state.auth.login
 })
 
-export default connect(mapStateToProps, {setUserData})(HeaderContainer);
+export default connect(mapStateToProps, {authMe})(HeaderContainer);
