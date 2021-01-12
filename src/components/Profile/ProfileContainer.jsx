15b6f90be19838d2728a09ca37474998c9de0c03
Roman <mister02.rv@gmail.com>
2021-01-12 23:39:54 +0200

    refactor

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,9 +4,8 @@ import React from 'react'
 import Profile from './Profile';
 import * as axios from 'axios'
 import { connect } from 'react-redux';
-import {setUserProfile} from '../../redux/profile-reducer'
+import {setUserProfile, getProfile} from '../../redux/profile-reducer'
 import { withRouter } from 'react-router-dom';
-import {usersAPI} from '../../api/api'
 
 class ProfileContainer extends React.Component {
     constructor(props) {
@@ -14,8 +13,7 @@ class ProfileContainer extends React.Component {
     }
 
     componentDidMount() {
-        usersAPI.getProfile(this.props.match.params.userId)
-            .then(response => this.props.setUserProfile(response.data))
+        this.props.getProfile(this.props.match.params.userId)
     }
 
     render() {
@@ -27,4 +25,6 @@ const mapStateToProps = state => ({
     profile: state.profilePage.profile
 })
 
-export default connect(mapStateToProps, {setUserProfile})(withRouter(ProfileContainer));
+export default connect(mapStateToProps, {
+    setUserProfile, getProfile
+})(withRouter(ProfileContainer));
